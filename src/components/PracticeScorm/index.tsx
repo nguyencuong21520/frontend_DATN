ed3a049071fccab5ce3dceadb1c62022266e72b4
khoatranpc <khoatranpc603@gmail.com>
2022-11-24 15:30:44 +0700

    INIT LAYOUT SCORM

---
+++ src/components/PracticeScorm/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+export const PracticeScorm = () => {
+    return (
+        <div className="container-preview-scorm">
+            <div className="title">
+                <span>Excel {'>'} Thực hành 01: Mở tập tin</span>
+            </div>
+            <div className="main">
+                <div className="main-scorm"></div>
+                <div className="right-fnc"></div>
+            </div>
+        </div>
+    )
+}
